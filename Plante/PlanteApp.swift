87ea937ce550d8b052e77771d5344062dead7ebc
TiPSO <pakawat.tips@gmail.com>
2022-04-15 21:11:51 +0700

    Initial Commit

---
+++ Plante/PlanteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanteApp.swift
+//  Plante
+//
+//  Created by Tipso on 15/4/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlanteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
